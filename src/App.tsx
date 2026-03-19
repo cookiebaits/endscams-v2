bf00717fae5008db9e584d421bae5f6bce7faae6
C B <cookiescambaits@gmail.com>
2026-03-19 11:54:45 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ function App() {
                       <Route path="/tracker" element={<TrackerPage />} />
                       <Route path="/report" element={<ReportScamPage />} />
                       <Route path="/education" element={<EducationPage />} />
-                      <Route path="/ftc-scams" element={<FTCScamsPage />} />
+                      <Route path="/ftc-scams" element={<TriagePage />} />
                     </Routes>
                   </div>
                   <Footer />
